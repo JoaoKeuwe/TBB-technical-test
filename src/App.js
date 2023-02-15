97c8464d08ae10f8d55d360be8c0dc2fb407a6fd
JoaoKeuwe <joaov2872@hotmail.com>
2023-02-15 19:51:14 -0300

    refactor: :bug: refactoring files

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import {api} from './services/mocks/output'
 
 function App() {
  
-  useEffect(() => { console.log(api.data.nodes.map((name) => name.name))}, []);
-  return <div className="App">
-    
-  </div>;
+  // useEffect(() => { console.log(api.data.nodes.map((name) => name.name))}, []);
+  // return <div className="App">
+  //     </div>;
+
 }
 
 export default App;
