75b88775f6284a26104bf1e7d356919132bff16d
JoaoKeuwe <joaov2872@hotmail.com>
2023-02-15 19:57:41 -0300

    fix:

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,8 +1,13 @@
 import React, { useState } from 'react'
 import { api } from '../services/mocks/output'
+import IconGlass from '../icons/glass.svg'
 
 function Filters({ setListProducts }) {
     const [count, setCount] = useState({})
+    const [searchBar, setSearchBar] = useState()
+    // const [listProducts, setListProducts] = useState(api.data.nodes)
+    const [productsBackup, setProductsBackup] = useState(api.data.nodes)
+
 
     const handleFilterByCategory = (event) => {
         const products = api.data.nodes
@@ -10,9 +15,9 @@ function Filters({ setListProducts }) {
         setListProducts(filterProducts)
         console.log('sss',)
     }
-    
+
     const categories = (arrProducts) => {
-        
+
         const arrayNames = arrProducts.map((product) => product.category.name);
         arrayNames.forEach((nameProduct) => {
             if (count[nameProduct]) {
@@ -25,8 +30,35 @@ function Filters({ setListProducts }) {
 
     }
 
+    const handleFilter = (event) => {
+        setSearchBar(event.target.value);
+    }
+
+    const handleSearch = (event) => {
+        event.preventDefault()
+        const filterProducts = productsBackup.filter((product, i) => product.category.name.toLowerCase().includes(searchBar.toLowerCase()));
+        setListProducts(filterProducts)
+    }
+
     return (
         <div>
+
+            <form
+                onSubmit={handleSearch}>
+
+                <input
+                    onChange={handleFilter}
+                    type="text" placeholder='BUSQUE AQUI' />
+
+                <button
+                    type='submit'
+
+                >
+                    <img width={10} src={IconGlass} alt="" />
+                </button>
+
+            </form>
+
             {categories(api.data.nodes).map((product) => (
                 <section>
                     <label htmlFor={product} >
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,45 +1,45 @@
 import React, { useState } from 'react'
 import Results from '../components/Results'
-import IconGlass from '../icons/glass.svg'
+// import IconGlass from '../icons/glass.svg'
 import { api } from '../services/mocks/output'
 
 import Filters from '../components/Filters'
 function Home() {
-  const [searchBar, setSearchBar] = useState()
+//    const [searchBar, setSearchBar] = useState()
   const [listProducts, setListProducts] = useState(api.data.nodes)
-  const [productsBackup, setProductsBackup] = useState(api.data.nodes)
+//   const [productsBackup, setProductsBackup] = useState(api.data.nodes)
 
 
-  const handleFilter = (event) => {
-    setSearchBar(event.target.value);
-  }
+  // const handleFilter = (event) => {
+  //   setSearchBar(event.target.value);
+  // }
 
-  const handleSearch = (event) => {
-    event.preventDefault()
-    const filterProducts = productsBackup.filter((product, i) => product.category.name.toLowerCase().includes(searchBar.toLowerCase()));
-    setListProducts(filterProducts)
-  }
+  // const handleSearch = (event) => {
+  //   event.preventDefault()
+  //   const filterProducts = productsBackup.filter((product, i) => product.category.name.toLowerCase().includes(searchBar.toLowerCase()));
+  //   setListProducts(filterProducts)
+  // }
 
   return (
     <div>
       <header>
         <h1> O QUE VOCÊ <span>ESTÁ PROCURANDO?</span></h1>
       </header>
-      <form 
-      onSubmit={handleSearch}>
+      {/* <form
+        onSubmit={handleSearch}>
 
         <input
           onChange={handleFilter}
           type="text" placeholder='BUSQUE AQUI' />
 
         <button
-        type='submit'
-          
+          type='submit'
+
         >
           <img width={10} src={IconGlass} alt="" />
         </button>
 
-      </form>
+      </form> */}
 
       <section>
         <h2>filtros</h2>
